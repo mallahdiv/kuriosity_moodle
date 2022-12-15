9c55dd4c1a6127802ae1e429c94e3fdadff4dffb
Caden Hong <caden.p.hong@gmail.com>
2022-12-14 21:32:56 -0500

    v36 with info.php

--- moodle_infrastructure/ecs.tf
+++ moodle_infrastructure/ecs.tf
@@ -47,7 +47,7 @@ resource "aws_ecs_task_definition" "moodle_ecs_task" {
   [
     {
       "name": "moodle-container",
-      "image": "kuragroup4/kuriosity:1.35",
+      "image": "kuragroup4/kuriosity:1.36",
       "logConfiguration": {
         "logDriver": "awslogs",
         "options": {
