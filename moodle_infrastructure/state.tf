2e8025f24bdc33e872429ab7a716d42db847d45d
cadenhong <caden.p.hong@gmail.com>
2022-12-16 15:52:09 -0500

    change backend s3 key

--- moodle_infrastructure/state.tf
+++ moodle_infrastructure/state.tf
@@ -2,7 +2,7 @@
 terraform {
  backend "s3" {
    bucket         = "moodle-tfstate-bucket"
-   key            = "state/terraform.tfstate"
+   key            = "terraform.tfstate"
    region         = "us-east-1"
    encrypt        = true
    kms_key_id     = "alias/moodle-tf-bucket-key"
